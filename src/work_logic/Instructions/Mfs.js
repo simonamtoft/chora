f9a609e297b4adfe4625a12cb829d6ab33382352
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-02-26 23:35:35 +0100

    Move from and to instructions

---
+++ src/work_logic/Instructions/Mfs.js
@@ -0,0 +1,32 @@
+import { parseNum, parseReg } from '../../../helper'
+
+export const compile_reg = (pred, rd, ss) => {
+    let binary = [0];
+
+    pred = parseNum(pred);
+    rd = parseReg(rd);
+    ss = parseReg(ss); 
+
+    binary[0] |= pred << 27;
+    binary[0] |= 0b01001 << 22;
+    binary[0] |= rd << 17;
+    binary[0] |= 0b011 << 4;
+    binary[0] |= ss << 0;
+
+    return binary;
+};
+
+class Mfs {
+    constructor(pred, rd, ss) {
+        this.pred = pred;
+        this.rd = rd;
+        this.ss = ss;
+        this.memory = compile_reg(pred, rs1, sd);
+    }
+
+    execute() {
+        reg[this.rd] = reg[this.ss]
+    }
+}
+
+export default Mfs
---
+++ src/work_logic/Instructions/Mts.js
@@ -0,0 +1,33 @@
+import { parseNum, parseReg } from '../../../helper'
+
+export const compile_reg = (pred, rs1, sd) => {
+    let binary = [0];
+
+    pred = parseNum(pred);
+    rs1 = parseReg(rs1);
+    sd = parseReg(sd); 
+
+    binary[0] |= pred << 27;
+    binary[0] |= 0b01001 << 22;
+    binary[0] |= rs1 << 12;
+    binary[0] |= imm << 7;
+    binary[0] |= 0b010 << 4;
+    binary[0] |= sd << 0;
+
+    return binary;
+};
+
+class Mts {
+    constructor(pred, rs1, sd) {
+        this.pred = pred;
+        this.rs1 = rs1;
+        this.sd = sd;
+        this.memory = compile_reg(pred, rs1, sd);
+    }
+
+    execute() {
+        reg[this.sd] = reg[this.rs1]
+    }
+}
+
+export default Mts
