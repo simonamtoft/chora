ecd0f7cde6f74ee4f84d4eec5ac3d3e6ebf2e62c
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-02-26 23:04:04 +0100

    Predicate instructions

---
+++ src/work_logic/Instructions/Predicate/Pxor.js
@@ -0,0 +1,17 @@
+import { compile_reg } from "./compilers";
+
+class Pxor {
+    constructor({ pred, pd, ps1, ps2 }) {
+        this.pd = pd;
+        this.ps1 = ps1;
+        this.ps2 = ps2;
+        this.func = 0b0110;
+        this.binary = compile_reg(pred, pd, ps1, ps2, this.func)
+    }
+
+    execute( { reg } ) {
+        reg[this.pd] = reg[this.ps1] ^ reg[this.ps2]
+    }
+}
+
+export default Pxor;
---
+++ src/work_logic/Instructions/Predicate/index.js
@@ -0,0 +1,5 @@
+import Pand from './Pand'
+import Pxor from './Pxor'
+import Por from './Por'
+
+export default { Pand, Pxor, Por }
