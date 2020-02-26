75c369fe82f1b656f101e467bcad411a00963932
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-02-26 18:20:42 +0100

    compiler logic for Compare

---
+++ src/work_logic/Instructions/Compare/compilers.js
@@ -0,0 +1,40 @@
+import { toNum, toOther } from '../../../helper'
+
+export const compile_reg = (pred, pd, rs1, rs2, func) => {
+    let binary = [0];
+
+    pred = toNum(pred);
+    func = toNum(func);
+    pd = toOther(pd);
+    rs1 = toOther(rs1);
+    rs2 = toOther(rs2);
+
+    binary[0] |= pred << 27;
+    binary[0] |= 0b01000 << 22;
+    binary[0] |= rd << 17;
+    binary[0] |= rs1 << 12;
+    binary[0] |= rs2 << 7;
+    binary[0] |= 0b011 << 4;
+    binary[0] |= func << 0;
+    return binary;
+};
+
+export const compile_imm = (pred, func, pd, rs1, imm) => {
+    let binary = [0];
+
+    pred = toNum(pred);
+    func = toNum(func);
+    imm = toNum(imm);
+    pd = toOther(pd);
+    rs1 = toOther(rs1);
+    
+    binary[0] |= pred << 27;
+    binary[0] |= 0b01000 << 22;
+    binary[0] |= rd << 17;
+    binary[0] |= rs1 << 12;
+    binary[0] |= imm << 7;
+    binary[0] |= 0b110 << 4;
+    binary[0] |= func << 0;
+    return binary;
+};
+
