d85d20f1fe2144fff6cd2434b8de63044ca0ea66
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2020-02-27 11:11:57 +0100

    make the helper actually export

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
-function parseNum(input) {
+export function parseNum(input) {
     return typeof input == "number" ? input : Number(input);
 }
 
-function parseReg(input) {
+export function parseReg(input) {
     return typeof input == "number" ? input : Number(input.match(/\d+/i)[0]);
 }
