ef0a98e0c48cf41a6d303ed85bf2ec04c546ed76
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-02-27 10:15:31 +0100

    Initial CPU

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import './css/App.css';
+
+import CPU from './work_logic/CPU/CPU'
+
 
 function App() {
   return (
     <div className="App">
+      <CPU/>
     </div>
   );
 }
