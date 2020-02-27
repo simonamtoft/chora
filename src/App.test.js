8e01029bdbfce1277de7bcab3d3609d6c64ca0b5
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2020-02-27 17:57:40 +0100

    Test for BinaryArithmetics.Add

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
+    const linkElement = getByText(/./i);
     expect(linkElement).toBeInTheDocument();
 });
