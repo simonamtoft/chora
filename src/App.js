988165f94657065771c8463f2c4d8cc008c552e7
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-02-28 12:21:10 +0100

    Added execution buttons
    Next, execute all instructions and reset instructions.
    Still misses to implement the work_logic behind it.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
         //let reg = new Registers();
 
         this.state = {
-            editor: "",
-            instque: "",
+            instructions: "",
+            instCount: 0, 
             inst: {
                 type: "",
                 rd: "",
@@ -22,34 +22,70 @@ class App extends Component {
         };
     }
 
-    parseInputToInstruction= () => {
-        // Decode next instruction in queue
-        let instnext = this.state.instque.split("\n")[0];
-        let [type, rd, r1, op2] = instnext.split(" ");
+    getUserInput = (instructions) => {
+        this.setState({instructions: instructions});
+    }
 
-        // Delete first line
-        this.setState({instque: this.state.instque.split("\n").slice(1).join("\n")});
-        
-        // Set state 
+    executeNextInstruction = () => {
+
+        // Check if there is anything to be executed
+        let que = this.state.instructions;
+        if ( que === "" || que == null) {
+            console.log("Error: Instruction queue is empty.");
+            return -1;
+        } else if (this.state.instCount === que.split(/\r\n|\r|\n/).length) {
+            console.log("Error: All instructions executed.");
+            return -1;
+        }
+
+        // Increment counter
+        this.setState((prevState) => ({
+            instCount: prevState.instCount + 1
+        }));
+
+        // Decode and set next instruction
+        let instnext = que.split("\n")[this.state.instCount];
+        let [type, rd, r1, op2] = instnext.split(" ");
         this.setState({type : type, rd: rd, r1: r1, op2: op2});
 
-        console.log(this.state.instque);
+        // Execute Instruction
+        console.log(`Instruction: ${type} ${rd} ${r1} ${op2} executed`);  
     }
 
-    getUserInput = (instructions) => {
-        this.setState({editor: instructions, instque: instructions});
+    executeInstructions = () => {
+
+        // Check if there is anything to be executed
+        let que = this.state.instructions;
+        let queLength = que.split(/\r\n|\r|\n/).length;
+        if ( que === "" || que == null) {
+            console.log("Error: Instruction queue is empty.");
+            return -1;
+        } else if (this.state.instCount == queLength) {
+            console.log("Error: All instructions already executed");
+            return -1;
+        }
+        
+        // Execute remaining instructions
+        for (let i = this.state.instCount; i < queLength; i++) {
+            this.executeNextInstruction();
+        }
     }
 
-    
+    resetExecution = () => {
+        this.setState({type : "", rd: "", r1: "", op2: "", instCount: 0});
+        console.log("Reset");
+    }
+
 
     render() {
         return (
             <div className="App">
-                <UserEditor parentCallback = {this.getUserInput}/>
-                <div>Type  rd r1   op2</div>
-                <div>{this.state.type} {this.state.rd} {this.state.r1}   {this.state.op2}</div>
-
-                <button onClick={this.parseInputToInstruction}>Convert</button>
+                <UserEditor parentCallback = {this.getUserInput}/>      
+                <div>
+                    <button onClick={this.executeNextInstruction}>Next</button>
+                    <button onClick={this.executeInstructions}>Execute Instructions</button>
+                    <button onClick={this.resetExecution}>Reset</button>
+                </div>             
             </div>
         );
     }
