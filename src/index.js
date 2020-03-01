d2e53f64b770beef447c4b01fc06af935607b57e
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-03-01 22:52:09 +0100

    delete generated stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./css/index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from "./App"; 
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-serviceWorker.unregister();
