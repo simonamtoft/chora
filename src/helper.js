6460997c46399d5ff0a613d93b9684c16eceff35
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-03-05 13:31:27 +0100

    remove unused import

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,3 @@
-import { render } from "@testing-library/react";
-import React from "react";
-
 export const parseNum = (input) => typeof input == "number" ? input : Number(input);
 export const parseReg = (input) => typeof input == "number" ? input : Number(input.match(/\d+/i)[0]);
 export const toUint32 = (n) => n >>> 0;
