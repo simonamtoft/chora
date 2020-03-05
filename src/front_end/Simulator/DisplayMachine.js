88db2a994d832d25a9c647fb7ca6a386b0e41610
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-03-05 15:51:06 +0100

    table example

--- src/front_end/Simulator/DisplayMachine.js
+++ src/front_end/Simulator/DisplayMachine.js
@@ -3,11 +3,19 @@ import React, { Fragment } from "react";
 const DisplayMachine = () => {
 	return (
 		<Fragment>
-			<textarea
-				className = "col-12"
-				placeholder = "Machine Code"
-				readOnly
-			/>	
+			<table className="table table-striped table-sm">
+				<thead>
+					<tr>
+						<th scope="col">Machine Code</th>
+						<th scope="col">Basic Code</th>
+						<th scope="col">Original Code</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+
+				</tbody>
+			</table>
 		</Fragment>
 	)
 }
