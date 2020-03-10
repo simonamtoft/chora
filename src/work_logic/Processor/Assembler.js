531e5e657ae6b428a6ec1afde478db59e7840fe4
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-03-10 18:58:38 +0100

    skip uncommented lines

--- src/work_logic/Processor/Assembler.js
+++ src/work_logic/Processor/Assembler.js
@@ -6,17 +6,22 @@ class Assembler {
 	}
 
 	generateInstQue(editor) {
-		let instQue = [];
-		let queLength = editor.split(/\r\n|\r|\n/).length;
-		let inst;
+		let lineCount = editor.split(/\r\n|\r|\n/).length;
+		let line, commentCount = 0;
+		editor = editor.split("\n");
+
+		// Loop over all editor lines
+		for (let i = 0; i < lineCount; i++) {
+			line = editor[i];
+
+			if (!isComment(line)) {
+				this.instQue[i-commentCount] = parseInst(line);
+			} else {
+				commentCount += 1;
+			}
+		}
 	
-		for (let i = 0; i < queLength; i++) {
-			inst = editor.split("\n")[i];
-			instQue[i] = parseInst(inst);
-		} 
-	
-		this.instQue = instQue;
-		this.queLength = instQue.length;
+		this.queLength = this.instQue.length;
 	}
 
 	/**
@@ -36,9 +41,21 @@ class Assembler {
 	}
 }
 
+// Check if line is a comment
+const isComment = (line) => {
+	let parsedInst = parseInst(line); 
+
+	if (parsedInst[0] === "#") {
+		return true;
+	}
+	return false;
+}
+
 const parseInst = (inst) => {
 	let parsedInst = inst.trim();
-	parsedInst = parsedInst.replace("=", "").replace("[", "").replace("]", "").replace("+", "");
+	// Replace special characters. But keep comment tag to skip commented out lines.
+	// eslint-disable-next-line
+	parsedInst = parsedInst.replace(/[=+\[\]]/g, "").replace("#", "# "); 
 	// eslint-disable-next-line
 	let [type, des, s1, s2] = parsedInst.split(/[ 	,]+/);
 	return [type, des, s1, s2];
