d773e4f59fcd632db219db09c9eb7b5a28c63bf7
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-04-10 16:02:32 +0200

    codemirror

--- src/Front End/Editor.js
+++ src/Front End/Editor.js
@@ -1,41 +1,37 @@
-import React, {Component, Fragment } from "react";
+import React, { Component } from "react";
+import CodeMirror from "react-codemirror";
 import PropTypes from "prop-types";
+import "../../node_modules/codemirror/lib/codemirror.css";
 import "../CSS/Editor.css";
 
-/**
- * Editor: Handles the "Editor" tab. Displays a textinput where the code input is returned.
- * @param {func} props.editorUpdate - Function trigger when user updates the input to the editor
- */
-class Editor extends Component {
+class Editor extends Component{
 	constructor(props) {
 		super(props);
-		this.state = {editor: ""};
-		this.textEditor = React.createRef();
+		this.state = {
+			code: "",
+			readOnly: false,
+			mode: "markdown",
+		};
+
+		this.updateCode = this.updateCode.bind(this);
 	}
 
-handleEditorChange = (event) => {
-	this.setState({editor: event.target.value});
-	this.props.editorUpdate(event.target.value);
-}
+	updateCode (newCode) {
+		this.setState({
+			code: newCode,
+		});
+		this.props.editorUpdate(newCode);
+	}
 
-componentDidMount() {
-	this.textEditor.current.focus();
-}
-
-render() {
-	const {editor} = this.state;
-
-	return(
-		<Fragment>
-			<textarea 
-				className = "col-12 editor"
-				ref={this.textEditor}
-				value={editor}
-				onChange={this.handleEditorChange}>
-			</textarea>  
-		</Fragment>
-	);
-}
+	render() {
+		let options = {
+			mode: this.state.mode,
+			lineNumbers: true,
+		};
+		return(
+			<div><CodeMirror value={this.state.code} onChange={this.updateCode} options={options} autoFocus={true}/></div>
+		);
+	}
 }
 
 Editor.propTypes = {
