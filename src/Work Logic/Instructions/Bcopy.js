9cce18012b7256c10e4d98db51b780d84011e159
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2020-04-22 15:07:56 +0200

    bcopy changes

--- src/Work Logic/Instructions/Bcopy.js
+++ src/Work Logic/Instructions/Bcopy.js
@@ -7,7 +7,7 @@ export const compile_reg = (pred, rd, rs1, imm, ps) => {
 	imm = parseNum(imm);
 	rd = parseReg(rd);
 	rs1 = parseReg(rs1);
-	ps = parseReg(ps); 
+	ps = ps[0] === "!" ? 0b1000 | parseReg(ps) : parseReg(ps);
 
 	binary[0] |= pred << 27;
 	binary[0] |= 0b01000 << 22;
@@ -34,13 +34,12 @@ class Bcopy {
      * @param {number}          fields.imm  - Second operand, immediate value.
      * @param {string}          fields.ps   - Predicate register. (can be negated)
      */
-	constructor({pred, rd, rs1, imm, neg, ps}) {
+	constructor({pred, rd, rs1, imm, ps}) {
 		this.name = "bcopy";
 		this.pred = pred;
 		this.rd = rd;
 		this.rs1 = rs1;
 		this.imm = imm & 0x1F;
-		this.neg = neg;
 		this.ps = ps; 
 		this.binary = compile_reg(pred, rd, rs1, imm, ps); 
 	}
@@ -56,7 +55,7 @@ class Bcopy {
 	}
 
 	toString(){
-		return `${this.pred ? `(${this.pred&0b1000 ? "!" : ""}p${this.pred&0b0111}) ` : ""}${this.name} ${this.rd} = ${this.rs1}, ${this.imm}, ${this.neg ? "!" : ""}${this.ps}`;
+		return `${this.pred ? `(${this.pred&0b1000 ? "!" : ""}p${this.pred&0b0111}) ` : ""}${this.name} ${this.rd} = ${this.rs1}, ${this.imm}, ${this.ps}`;
 	}
 
 }
