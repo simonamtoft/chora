25054c4b47241a23e4f5332e48e5ad029d0e0393
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2020-04-24 16:58:43 +0200

    Update DisplayCode.js

--- src/Front End/Simulator/DisplayCode.js
+++ src/Front End/Simulator/DisplayCode.js
@@ -56,7 +56,7 @@ const MachineRow = (bundle, pc, addr) => {
 	for(let i of bundle){
 		rows.push(
 			<tr key={idx} className={color} >
-				<td>{intToHex(i.instruction.binary[0])}</td>
+				<td>{intToHex(i.instruction.binary[0], 8)}</td>
 				<td>{i.instruction.toString()}</td>
 				<td>{i.original}</td>
 			</tr>
