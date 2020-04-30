fa54f6b84bb932e3a6e6ea56f5962702988c6ee3
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2020-04-30 22:02:27 +0200

    Upload files

---
+++ precache-manifest.fd464fbf51ce51cddcf39e91c86e4195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4e19f87a82d74107c56b978184d7c8",
+    "url": "/chora/index.html"
+  },
+  {
+    "revision": "2293832bbfe4a208b94c",
+    "url": "/chora/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "36572d9238ba7970936d",
+    "url": "/chora/static/css/main.96453466.chunk.css"
+  },
+  {
+    "revision": "2293832bbfe4a208b94c",
+    "url": "/chora/static/js/2.24da4f73.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/chora/static/js/2.24da4f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36572d9238ba7970936d",
+    "url": "/chora/static/js/main.969166fe.chunk.js"
+  },
+  {
+    "revision": "55a124a44a64bd69e867",
+    "url": "/chora/static/js/runtime-main.2c7a1063.js"
+  }
+]);
